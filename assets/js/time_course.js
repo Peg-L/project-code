18d1af729d5cfff8de8dfa2fbcc57de2f02ca5dd
龔楙訓 <vancat@gongmaoxundeMacBook-Air.local>
2023-11-06 10:22:51 +0800

    feat/update attend courses

--- assets/js/time_course.js
+++ assets/js/time_course.js
@@ -1,11 +1,23 @@
+//判斷早中晚之參數
 const morning = document.querySelector('#morning');
 const afternoon = document.querySelector('#afternoon');
 const evening = document.querySelector('#evening');
-
+let clickTime = '';
 let morning_str = '';
 let afternoon_str = '';
 let evening_str = '';
 
+//add data to db
+const attendSubmit = document.querySelector('#attendSubmit');
+let oldAttendTime = [];
+attendSubmit.addEventListener('click',() => {
+    //console.log(clickCourse,clickDay,userId,clickTime);
+    postAttendCourse(clickCourse,clickDay,userId,clickTime);
+});
+
+
+
+//顯示教師當日開放時間
 function viewTimeCourse(){
     if (clickCourse!==""&&clickDay!==""&&userId!==""){
         console.log('gooooood');
@@ -17,13 +29,13 @@ function viewTimeCourse(){
                     viewTime.forEach(item => {
                         switch (classifyTime(item)) {
                             case '上午':
-                                morning_str += `<li class="btn-time">${item}</li>`;
+                                morning_str += `<li class="btn-time" data-time=${item}>${item}</li>`;
                                 break;
                             case '中午':
-                                afternoon_str+=`<li class="btn-time">${item}</li>`;
+                                afternoon_str+=`<li class="btn-time" data-time=${item}>${item}</li>`;
                                 break;
                             case '晚上':
-                                evening_str+=`<li class="btn-time">${item}</li>`;
+                                evening_str+=`<li class="btn-time" data-time=${item}>${item}</li>`;
                                 break;
                         }
                     });
@@ -34,13 +46,49 @@ function viewTimeCourse(){
                 afternoon_str = '';
                 evening.innerHTML = evening_str;
                 evening_str = '';
+                const btn_times = document.querySelectorAll('.btn-time');
+                btn_times.forEach(btn=>{
+                    btn.addEventListener('click',e=>{
+                        btn_times.forEach(btn=>{
+                            btn.classList.remove('active');
+                        })
+                        e.currentTarget.classList.add('active');
+                        clickTime = e.target.getAttribute('data-time');
+                    })
+                })
         })
     }else{
         return;
     }
 }
-    
-
+//將資料加入到db
+function postAttendCourse(clickCourse,clickDay,userId,clickTime){
+    const _url = 'http://localhost:3000';
+    const data = {
+        courseId : clickCourse,
+        date : clickDay,
+        time : clickTime
+    }
+    axios.get(`${_url}/user_courses/${userId}`)
+  .then(response => {
+    oldAttendTime = [...response.data.attendTime];
+    //console.log(oldAttendTime);
+    //update data to db
+    axios.patch(`${_url}/user_courses/${userId}`,{
+        attendTime : [...oldAttendTime , data]
+    })
+    .then(response => {
+        console.log('add success');
+    })
+    .catch(error => {
+        console.error('Error adding post:', error);
+    });
+  })
+  .catch(error => {
+    console.error('Error adding post:', error);
+  });
+}
+//判斷時間為早、中、晚
 function classifyTime(timeString) {
     const time = new Date(`2000-01-01 ${timeString}`);
     const hours = time.getHours();
