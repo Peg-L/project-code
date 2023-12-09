238bf4e10acdd202b57a11988ad1604b8d2012ac
Peggy Lin <stu90233@gmail.com>
2023-12-09 16:55:23 +0800

    Updates

--- assets/js/fold.js
+++ assets/js/fold.js
@@ -1,19 +1,19 @@
-const bigFold = document.querySelectorAll('#largeFold');
-const icon_arrow = document.querySelectorAll('.arrow');
+const bigFold = document.querySelectorAll("#largeFold");
+const icon_arrow = document.querySelectorAll(".arrow");
 
-const littleFold = document.querySelectorAll('#littleFold');
-const icon_plus = document.querySelectorAll('.plus');
+const littleFold = document.querySelectorAll("#littleFold");
+const icon_plus = document.querySelectorAll(".plus");
 
-bigFold.forEach((item,i) => {
-    item.addEventListener('click',e => {
-        icon_arrow[i].classList.toggle('fa-chevron-up')
-        icon_arrow[i].classList.toggle('fa-chevron-down');
-    })
-})
+bigFold.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    icon_arrow[i].classList.toggle("fa-chevron-up");
+    icon_arrow[i].classList.toggle("fa-chevron-down");
+  });
+});
 
-littleFold.forEach((item,i) => {
-    item.addEventListener('click',e => {
-        icon_plus[i].classList.toggle('fa-plus')
-        icon_plus[i].classList.toggle('fa-minus');
-    })
-})
+littleFold.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    icon_plus[i].classList.toggle("fa-plus");
+    icon_plus[i].classList.toggle("fa-minus");
+  });
+});
