c123bbcfa50793ec3b5ddb72f8f460d4a99139ff
Peggy Lin <stu90233@gmail.com>
2023-12-10 19:24:41 +0800

    Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,46 @@
 import axios from "axios";
 
+// 圖片 src 修改
+import treasureImgUrl from "../images/category/treasure.png";
+document.getElementById("treasureImg1").src = treasureImgUrl;
+document.getElementById("treasureImg2").src = treasureImgUrl;
+
+import frontendImgUrl from "../images/category/frontend.svg";
+document.getElementById("frontendImg").src = frontendImgUrl;
+
+import backendUrl from "../images/category/backend.svg";
+document.getElementById("backendUrl").src = backendUrl;
+
+import databaseUrl from "../images/category/database.svg";
+document.getElementById("databaseUrl").src = databaseUrl;
+
+import securityUrl from "../images/category/security.svg";
+document.getElementById("securityUrl").src = securityUrl;
+
+import testingUrl from "../images/category/testing.svg";
+document.getElementById("testingUrl").src = testingUrl;
+
+import UIUXUrl from "../images/category/UI-UX.svg";
+document.getElementById("UIUXUrl").src = UIUXUrl;
+
+import devOpsUrl from "../images/category/DevOps.svg";
+document.getElementById("devOpsUrl").src = devOpsUrl;
+
+import mobileUrl from "../images/category/mobile.svg";
+document.getElementById("mobileUrl").src = mobileUrl;
+
+import AIUrl from "../images/category/AI.svg";
+document.getElementById("AIUrl").src = AIUrl;
+
+import gameUrl from "../images/category/game.svg";
+document.getElementById("gameUrl").src = gameUrl;
+
+import basicUrl from "../images/category/basic.svg";
+document.getElementById("basicUrl").src = basicUrl;
+
+import careerUrl from "../images/category/career.svg";
+document.getElementById("careerUrl").src = careerUrl;
+
 let bannerSwiper = new Swiper(".bannerSwiper", {
   slidesPerView: 1,
   loop: true,
