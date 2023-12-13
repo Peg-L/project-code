32b51006141f88e7d9e90b094d7aaff527adf6f9
Peggy Lin <stu90233@gmail.com>
2023-12-13 14:56:50 +0800

    feat: 🎸 optimize search input

--- assets/js/coursePage/search.js
+++ assets/js/coursePage/search.js
@@ -1,35 +1,35 @@
 import { data, getCoursesData } from "./api.js";
 
+/*** 取得 banner 搜尋框 ***/
+const courseSearchInput = document.querySelector(".course-search-input");
+
 /*** 首頁搜尋跳轉 ***/
 const indexSearchInput = localStorage.getItem("indexSearchInput");
 
 if (indexSearchInput) {
+  courseSearchInput.value = indexSearchInput;
   data.q = indexSearchInput;
   getCoursesData(data);
   data.q = "";
-  localStorage.removeItem("indexSearchInput");
 }
-
-/*** 取得 banner 搜尋框的值 ***/
-const courseSearchInput = document.querySelector(".course-search-input");
-
-let courseSearchInputValue = "";
-
-courseSearchInput.addEventListener("input", function () {
-  courseSearchInputValue = courseSearchInput.value;
-});
 
 const courseSearchButton = document.querySelector(".course-search-button");
 
 courseSearchButton.addEventListener("click", function () {
-  data.q = courseSearchInputValue;
+  data.q = courseSearchInput.value;
   getCoursesData(data);
-  courseSearchInput.value = "";
-  courseSearchInputValue = "";
+  localStorage.removeItem("indexSearchInput");
 });
 
 courseSearchInput.addEventListener("keyup", function (e) {
   if (e.key === "Enter") {
     courseSearchButton.click();
   }
+});
+
+// 清除輸入框
+const clearInputBtn = document.querySelector(".clear-input");
+
+clearInputBtn.addEventListener("click", function () {
+  courseSearchInput.value = "";
 });
